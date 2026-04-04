5198ce75de108648af819931555eeb446c8038dc
benjaminmandak0-del <benjamin.mandak0@gmail.com>
2026-04-04 11:08:48 +0200

    header

---
+++ cestovanie_do_japonska_php_projekt/templatemo_564_plot_listing/templates/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <title>Moja stránka</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.php" class="logo">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="category.php">Category</a></li>
+              <li><a href="listing.php" class="active">Listing</a></li>
+              <li><a href="contact.php">Contact Us</a></li> 
+              <li><div class="main-white-button"><a href="#"><i class="fa fa-plus"></i> Add Your Listing</a></div></li> 
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
