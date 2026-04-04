5c6443bb0f18e27aa62ef97ca8ac5e93b02a280b
benjaminmandak0-del <benjamin.mandak0@gmail.com>
2026-04-04 11:25:07 +0200

    footer

--- cestovanie_do_japonska_php_projekt/templatemo_564_plot_listing/category.php
+++ cestovanie_do_japonska_php_projekt/templatemo_564_plot_listing/category.php
@@ -381,64 +381,7 @@ https://templatemo.com/tm-564-plot-listing
     </div>
   </div>
 
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="about">
-            <div class="logo">
-              <img src="assets/images/black-logo.png" alt="">
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adicingi elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="helpful-links">
-            <h4>Helpful Links</h4>
-            <div class="row">
-              <div class="col-lg-6">
-                <ul>
-                  <li><a href="#">Categories</a></li>
-                  <li><a href="#">Reviews</a></li>
-                  <li><a href="#">Listing</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-6">
-                <ul>
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Awards</a></li>
-                  <li><a href="#">Useful Sites</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="contact-us">
-            <h4>Contact Us</h4>
-            <p>27th Street of New Town, Digital Villa</p>
-            <div class="row">
-              <div class="col-lg-6">
-                <a href="#">010-020-0340</a>
-              </div>
-              <div class="col-lg-6">
-                <a href="#">090-080-0760</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-12">
-          <div class="sub-footer">
-            <p>Copyright © 2021 Plot Listing Co., Ltd. All Rights Reserved.
-            <br>
-			Design: <a rel="nofollow" href="https://templatemo.com" title="CSS Templates">TemplateMo</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+<?php include 'templates/footer.php'; ?>
 
 
   <!-- Scripts -->
